f58fba32a6450ec92cf8d504f16bab772dabad98
juanpereyrajmp <juanpereyra.jmp@gmail.com>
2024-05-10 09:10:45 -0300

    Clases del proyecto anterior agregadas y creacion de la Default

---
+++ TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Manager/ImagenManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using manager;
+
+namespace manager
+{
+    public class ImagenManager
+    {
+
+        public List<Imagen> ListarImagenes()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id,IdArticulo,ImagenUrl From IMAGENES");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.Url = (string)datos.Lector["ImagenUrl"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+        public List<Imagen> listarPorIdArticulo (int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<Imagen> lista = new List<Imagen>();
+            try
+            {
+
+                datos.setearConsulta("Select Id,IdArticulo,ImagenUrl From IMAGENES WHERE IdArticulo=@idArticulo");
+                datos.setearParametro("@idArticulo", id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.Url = (string)datos.Lector["ImagenUrl"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+        
+        public void eliminarImagenes(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+
+                datos.setearConsulta("DELETE FROM IMAGENES WHERE Id = @Id");
+                datos.setearParametro("@Id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+
+    }
+}
