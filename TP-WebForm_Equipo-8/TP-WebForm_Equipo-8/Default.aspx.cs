c40c3543b614235a975e814020570969148f954a
Raul <raulvaleriorolon2024@gmail.com>
2024-05-14 00:18:32 -0300

    Agregado de tarjeta

--- TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Default.aspx.cs
+++ TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Default.aspx.cs
@@ -5,16 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using manager;
+using dominio;
 
 namespace TP_WebForm_Equipo_8
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Articulo> ListaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             ArticuloManager manager = new ArticuloManager();
-            dvgArticulos.DataSource = manager.ListarArticulosConSP();
-            dvgArticulos.DataBind();
+            //dvgArticulos.DataSource = manager.ListarArticulosConSP();
+            //dvgArticulos.DataBind();
+            ListaArticulos = manager.ListarArticulosConSP();
+            repArticulos.DataSource = ListaArticulos;
+            repArticulos.DataBind();
         }
     }
 }
