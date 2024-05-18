ddb47046e3b4c3ed59eb7a68d4659ab3b425b948
juanpereyrajmp <juanpereyra.jmp@gmail.com>
2024-05-18 08:45:14 -0300

    Reconfiguracion de forms y agregada correctamente la pagina de detalle

--- TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Default.aspx.cs
+++ TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Default.aspx.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using dominio;
+using manager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using manager;
-using dominio;
 
 namespace TP_WebForm_Equipo_8
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Home : System.Web.UI.Page
     {
         public List<Articulo> ListaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
@@ -22,74 +22,11 @@ namespace TP_WebForm_Equipo_8
                 repArticulos.DataSource = ListaArticulos;
                 repArticulos.DataBind();
             }
-            //if (Session["ListaArticulos"] == null)
-            //{
-            //    Session.Add("ListaArticulos", ListaArticulos);
-            //    repArticulos.DataSource = ListaArticulos;
-            //    repArticulos.DataBind();
-            //}
         }
 
-        protected void btnAgregarAlCarrito_Click(object sender, EventArgs e)
+        protected void btnVerDetalle_Click(object sender, EventArgs e)
         {
-            Button btn = (Button)sender;
-            int articuloId = Convert.ToInt32(btn.CommandArgument);
-            Carrito carrito = new Carrito();
 
-            ArticuloManager negocio = new ArticuloManager();
-            //ListaArticulos = negocio.listaParaImagenes();
-            ListaArticulos = negocio.ListarArticulos();
-
-
-            List<Articulo> seleccionados;
-            if (Session["Seleccionados"] == null)
-            {
-                seleccionados = new List<Articulo>();
-            }
-            else
-            {
-                seleccionados = (List<Articulo>)Session["Seleccionados"];
-            }
-
-            //foreach (Articulo item in ListaArticulos)
-            //{
-            //    // Si el ID del artículo coincide con el ID del artículo del botón, lo agrega a la lista de seleccionados
-            //    if (articuloId == item.Id)
-            //    {
-            //        // Agrega el artículo a la lista de seleccionados
-            //        seleccionados.Add(item);
-            //        // Detenemos la búsqueda una vez que el artículo se ha encontrado y agregado
-            //        break;
-            //    }
-            //}
-
-
-            foreach (Articulo item in ListaArticulos)
-            {
-                if (articuloId == item.Id)
-                {
-
-                    if (!seleccionados.Any(a => a.Id == item.Id))
-                    {
-                        seleccionados.Add(item);
-
-                    }
-
-                    break;
-                }
-            }
-
-
-            //foreach (Articulo item in ListaArticulos)
-            //{
-            //    if (articuloId == item.Id)
-            //  {
-            //       seleccionados.Add(item);
-            //    }
-            //}
-
-            Session["Seleccionados"] = seleccionados;
-            Response.Redirect(Request.RawUrl);
         }
     }
 }
---
+++ TP-WebForm_Equipo-8/TP-WebForm_Equipo-8/Detalle.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="Detalle.aspx.cs" Inherits="TP_WebForm_Equipo_8.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <link href="~/Content/Styles/styles.css" rel="stylesheet" type="text/css"/>
+    
+    <div class="text-center mt-4">
+        <h1>Articulos</h1>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <asp:Repeater ID="repArticulos" runat="server">
+            <ItemTemplate>
+                <div class="col">
+                    <div class="card shadow-sm" style="width: 100%; margin: auto;">
+                        <img src="<%# Eval("Imagen") %>" class="card-img-top" alt="Imagen del Articulo" style="height: 200px; object-fit: cover; margin: auto; width: auto;" onerror="this.onerror=null;this.src='https://www.svgrepo.com/show/508699/landscape-placeholder.svg';">
+                        <div class="card-body">
+                            <h5 class="card-title"><%#Eval("Nombre") %></h5>
+                            <p class="card-text"><%#Eval("Descripcion") %></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Marca:</strong> <%#Eval("Marca") %> </li>
+                            <li class="list-group-item"><strong>Categoria:</strong> <%#Eval("Categoria") %></li>
+                            <li class="list-group-item"><strong>Precio:</strong> <%#Eval("Precio") %></li>
+                        </ul>
+                        <div class="card-body text-center">
+                            <asp:Button ID="btnAgregarAlCarrito" runat="server" Text="Agregar al carrito" OnClick="btnAgregarAlCarrito_Click" CssClass="btn btn-success" CommandArgument='<%# Eval("Id") %>' CommandName="ArticuloId" />
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
+
